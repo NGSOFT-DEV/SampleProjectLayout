ee2e3286809c6646c41c0ac3751f96c1f2ad35b4
SharonRosner <sharosner@gmail.com>
2018-10-11 16:35:26 +0300

    Added login post method
    Added login post method:
    
    valid username and password are configurable via Settings.json

--- server/index.js
+++ server/index.js
@@ -10,10 +10,23 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing       applica
 
 var settings = JSON.parse(fs.readFileSync("Settings.json", 'utf8'));
 
+function authenticate(username, password) {
+    return (username == settings.username && password == settings.password);
+};
 
 app.get('/someGetMethod', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(app.data));
+});
+
+app.post('/login', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+
+    var data = req.body;
+    var username = data.username;
+    var password = data.password;
+
+    res.end(JSON.stringify(authenticate(username, password)));
 });
 
 app.use(express.static(settings.directoryToServe))
@@ -23,3 +36,5 @@ var server = app.listen(settings.httpPort, function() {
     var port = server.address().port;
     console.log("Server listening at http://%s:%s", host, port)
 });
+
+
